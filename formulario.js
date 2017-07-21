8efc65b24029692d4e7cc077695a6f88294d2846
Nicole Cavieres <nicole61192@gmail.com>
2017-07-21 07:25:14 -0400

    fin

--- formulario.js
+++ formulario.js
@@ -1,29 +1,13 @@
 
-/*
-document.getElementById("btnForm").addEventListener("submit",function a (){
-	var alerta = document.getElementById("alerta");
-	alerta.style.display="value";
-	alert("hoola")
-	});
-
-
-	
-*/
-
-var alerta = document.getElementById("alerta");
 var formulario=document.getElementById("formu");
 
+
 formulario.onsubmit = function(){
+	alert("Tu mensaje ha sido enviado");
 	ga('send', 'event', 'Form','submit');
 };
 
-	alerta.style.visibility="visible"
 
-
-var x = document.getElementById("x");
-x.onclick = function(){
-	alerta.style.visibility="hidden"
-}
 
 
 
